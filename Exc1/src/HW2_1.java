80485a34726a49357ca23bb8e64a9f569d92e84e
Gromova Juliya <gromovaji@yandex.ru>
2025-07-21 11:41:12 +0300

    Homework3 (test)

--- Exc1/src/HW2_1.java
+++ Exc1/src/HW2_1.java
@@ -12,9 +12,9 @@ public class HW2_1 {
         System.out.println("У Пети - " + randomItemPeter);
         if (randomItemVasya.equals(randomItemPeter)) {
             System.out.println("Результат игры: Ничья");
-        } else if ((randomItemVasya == "Бумага" && randomItemPeter == "Камень") ||
-                (randomItemVasya == "Ножницы" && randomItemPeter == "Бумага") ||
-                (randomItemVasya == "Камень" && randomItemPeter == "Ножницы")) {
+        } else if ((randomItemVasya.equals("Бумага") && randomItemPeter.equals("Камень")) ||
+                (randomItemVasya.equals("Ножницы") && randomItemPeter.equals("Бумага")) ||
+                (randomItemVasya.equals("Камень") && randomItemPeter.equals("Ножницы"))) {
             System.out.println("Результат игры: выиграл Вася");
         } else {
             System.out.println("Результат игры: выиграл Петя");
